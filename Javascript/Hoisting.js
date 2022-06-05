6f7a030ba6e37b0c7f09af738e3eb0b4ded52627
majed amria <majedamrya@hotmail.com>
2022-06-05 15:05:09 +0300

    Arrow

--- Javascript/Hoisting.js
+++ Javascript/Hoisting.js
@@ -1,67 +1,75 @@
-//1
-console.log(hello);                                   
-var hello = 'world';
+// //1
+// console.log(hello);                                   
+// var hello = 'world';
 
-//2
-var needle = 'haystack';
-test();
-function test(){
-    var needle = 'magnet';
-    console.log(needle);
-}
-
-//3
-var brendan = 'super cool';
-function print(){
-    brendan = 'only okay';
-    console.log(brendan);
-}
-console.log(brendan);
-
-//4
-var food = 'chicken';
-console.log(food);
-eat();
-function eat(){
-    food = 'half-chicken';
-    console.log(food);
-    var food = 'gone';
-}
-
-//5
-// mean();
-// console.log(food);
-// var mean = function() {
-//     food = "chicken";
-//     console.log(food);
-//     var food = "fish";
-//     console.log(food);
+// //2
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
 // }
+
+// //3
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+
+// //4
+// var food = 'chicken';
 // console.log(food);
+// eat();
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
 
-//6
-console.log(genre);
-var genre = "disco";
-rewind();
-function rewind() {
-    genre = "rock";
-    console.log(genre);
-    var genre = "r&b";
-    console.log(genre);
-}
-console.log(genre);
+// //5
+// // mean();
+// // console.log(food);
+// // var mean = function() {
+// //     food = "chicken";
+// //     console.log(food);
+// //     var food = "fish";
+// //     console.log(food);
+// // }
+// // console.log(food);
 
-//7
-dojo = "san jose";
-console.log(dojo);
-learn();
-function learn() {
-    dojo = "seattle";
-    console.log(dojo);
-    var dojo = "burbank";
-    console.log(dojo);
-}
-console.log(dojo);
+// //6
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+
+// //7
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo = "burbank";
+//     console.log(dojo);
+// }
+// console.log(dojo);
+
+param => {
+    let a = 1;
+    return a + param;
+  }
+
+  cos
+
 
 //8
 // console.log(makeDojo("Chicago", 65));
