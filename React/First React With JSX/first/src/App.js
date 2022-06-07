ab4c3d8a7e2a1d8287900ecc6f587663d43d7381
majed amria <majedamrya@hotmail.com>
2022-06-07 11:48:20 +0300

    JSX

---
+++ React/First React With JSX/first/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Dojo!</h1>
+      <h2>things i need to do..</h2>
+      <ul>
+        <li>Learn react</li>
+        <li>Climb MT. Everst</li>
+        <li>Run a Marthon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
